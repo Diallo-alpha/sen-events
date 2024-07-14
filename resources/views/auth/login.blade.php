afb103d61309dc5eb6866cd40e3d248344da68c0
Diallo-alpha <alphaloppecity@gmail.com>
2024-07-14 14:02:02 +0000

    responsives pages login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,36 +5,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 </head>
 <body>
-    <div class="container">
-        <div class="left-section">
-            <h1>SEN-EVENTS</h1>
-            <div class="social-links">
-                <a href="#" class="social-link"><i class="fab fa-instagram"></i></a>
-                <a href="#" class="social-link"><i class="fab fa-facebook-f"></i></a>
-                <a href="#" class="social-link"><i class="fab fa-twitter"></i></a>
+    <div class="container-fluid">
+        <div class="row vh-100">
+            <div class="col-lg-6 d-flex flex-column justify-content-center align-items-center left-section">
+                <h1>SEN-EVENTS</h1>
+                <div class="social-links">
+                    <a href="#" class="social-link"><i class="fab fa-instagram"></i></a>
+                    <a href="#" class="social-link"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="social-link"><i class="fab fa-twitter"></i></a>
+                </div>
             </div>
-        </div>
-        <div class="right-section">
-            <h2>CONNEXION</h2>
-            <form class="login-form" method="POST" action="{{ route('login') }}">
-                @csrf
-                <label for="email">Email</label>
-                <input type="email" id="email" name="email" placeholder="Email" value="{{ old('email') }}" required autofocus autocomplete="username">
-                <x-input-error :messages="$errors->get('email')" class="mt-2" />
-
-                <label for="password">Mot de passe</label>
-                <input type="password" id="password" name="password" placeholder="Mot de passe" required autocomplete="current-password">
-                <x-input-error :messages="$errors->get('password')" class="mt-2" />
-
-                <button type="submit">SE CONNECTER</button>
-            </form>
-            <div class="link-register">
-                <span><a href="{{ route('register') }}">S'inscrire</a></span>
+            <div class="col-lg-6 d-flex flex-column justify-content-center align-items-center right-section">
+                <h2>CONNEXION</h2>
+                <form class="login-form w-75" method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" id="email" name="email" class="form-control" placeholder="Email" value="{{ old('email') }}" required autofocus autocomplete="username">
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Mot de passe</label>
+                        <input type="password" id="password" name="password" class="form-control" placeholder="Mot de passe" required autocomplete="current-password">
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </div>
+                    <button type="submit" class="btn btn-primary">SE CONNECTER</button>
+                </form>
+                <div class="link-register mt-3">
+                    <span><a href="{{ route('register') }}">S'inscrire</a></span>
+                </div>
             </div>
         </div>
     </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
